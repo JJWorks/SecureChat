b059064178512759256774ab3c4fbf9c0f3e2fb8
JJ <kasaworks+git@gmail.com>
2018-10-09 08:55:43 -0700

    Added user Information to track who is in the chat.

---
+++ SecureChatWeb/UserProfile/UserInfoBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecureChatWeb.UserProfile
+{
+    /// <summary>
+    /// User Information Base in the Chat.
+    /// </summary>
+    public class UserInfoBase
+    {
+        /// <summary>
+        /// Creates an instance of UserInfoBase.
+        /// </summary>
+        public UserInfoBase() { }
+
+        /// <summary>
+        /// Gets a value of whether the user is verified.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if verified; otherwise, <c>false</c>.
+        /// </value>
+        public bool Verified { get; protected set; }
+
+        /// <summary>
+        /// Gets the name of the user.
+        /// </summary>
+        /// <value>
+        /// The name of the user.
+        /// </value>
+        public string UserName { get; protected set; }
+
+        /// <summary>
+        /// Gets/Sets the user Style
+        /// </summary>
+        public string Style { get; set; }
+
+
+        /// <summary>
+        /// Gets the Identifier for the user
+        /// </summary>
+        public string Identifier { get; protected set; }
+    }
+}
---
+++ SecureChatWeb/UserProfile/UserInformation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecureChatWeb.UserProfile
+{
+    /// <summary>
+    /// Sets the User Information.  Base of user InfoBase.
+    /// </summary>
+    public class UserInformation : UserInfoBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserInformation"/> class.
+        /// </summary>
+        /// <param name="_ConnectionID">Hubs ConnectionID.</param>
+        /// <param name="_UserName">Name of the user.</param>
+        public UserInformation(string _ConnectionID, string _UserName, string _Identifier, bool _Verified = false)
+        {
+            ConnectionID = _ConnectionID;
+            base.UserName = _UserName;
+            base.Verified = _Verified;
+            base.Identifier = _Identifier;
+        }
+
+        /// <summary>
+        /// Gets the Hub ConnectionID.
+        /// </summary>
+        /// <value>
+        /// The Hub ConnectionID.
+        /// </value>
+        public string ConnectionID { get; private set; }
+    }
+}
